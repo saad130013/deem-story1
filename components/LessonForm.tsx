543390cee1a23ccb894645b9002ac0ba36c3a693
saad130013 <saad130013@gmail.com>
2025-11-29 19:27:18 +0300

    Create LessonForm.tsx

---
+++ components/LessonForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LessonForm: React.FC = () => {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h2>Lesson Form</h2>
+      <p>هذه نسخة تجريبية من نموذج الدرس. سيتم استبدالها بالتصميم النهائي لاحقاً.</p>
+    </div>
+  );
+};
+
+export default LessonForm;
